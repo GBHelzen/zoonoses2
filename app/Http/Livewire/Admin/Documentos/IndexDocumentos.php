f09f3b8613943acfe89d218393801a5d2c9856c2
Gabriel <gbluckmann@outlook.com>
2023-12-06 12:58:05 -0400

    Update funcionando diretamente no Index

--- app/Http/Livewire/Admin/Documentos/IndexDocumentos.php
+++ app/Http/Livewire/Admin/Documentos/IndexDocumentos.php
@@ -4,13 +4,53 @@ namespace App\Http\Livewire\Admin\Documentos;
 
 use App\Models\Documento;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class IndexDocumentos extends Component
-{
+{   
+    use WithFileUploads;
+    public $arquivo;
+    public $nome_arquivo;
+    public $documento_id;
+    public $update = false;
     public $deleteModal  = false;
 
     public $documento = null;
     public $search = '';
+
+    public function updateDocumento($id)
+    {
+        $documento = Documento::find($id);
+
+        $this->documento_id = $documento->id;
+        $this->arquivo = $documento->arquivo;
+        $this->nome_arquivo = $documento->nome_arquivo;
+        $this->update=true;
+    }
+
+    public function updateData()
+    {
+
+        $this->validate([
+            'arquivo' => 'file|max:5120', // 5MB Max
+            'nome_arquivo' => 'required',
+        ]);
+        
+        $data = Documento::find($this->documento_id);
+
+        // Pegar o nome original do arquivo e armazena-lo na pasta docs
+        $arquivo = $this->arquivo->getClientOriginalName();
+        $this->arquivo->storeAs('docs', $arquivo);
+
+        $data->arquivo = $arquivo;
+        $data->nome_arquivo = $this->nome_arquivo;
+        $data->path = ('/storage/docs/' . basename($arquivo));
+        $data->save();
+
+        session()->flash('success', 'Documento ' . $data->nome_arquivo . ' editado com sucesso!');
+
+        return redirect()->route('documentos.index');
+    }
 
     public function openModal(Documento $documento)
     {
--- resources/views/admin/documentos/index-documentos.blade.php
+++ resources/views/admin/documentos/index-documentos.blade.php
@@ -1,4 +1,5 @@
     <x-slot name="header">
+        @if($update==false)
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Documentos') }}
         </h2>
@@ -55,7 +56,7 @@
                                     </div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap  text-center font-medium">
-                                    <a href="{{ route('documentos.edit', $documento) }}" class="text-indigo-600 hover:text-indigo-900">Editar</a>
+                                    <a href="#" wire:click="updateDocumento({{$documento['id']}})" class="text-indigo-600 hover:text-indigo-900">Editar</a>
                                     <a href="#" wire:click="openModal({{$documento}})" class="text-red-600 hover:text-red-900">Deletar</a>
                                 </td>
 
@@ -64,6 +65,60 @@
                             @endforeach
                         </tbody>
                     </table>
+
+        @else
+        <div class="shadow overflow-hidden sm:rounded-md">
+            <div class="px-4 py-5 bg-white sm:p-6">
+        
+        
+        
+                <div class="grid grid-cols-6 gap-6">
+        
+                    <div class="col-span-6 sm:col-span-3 ">
+                        <!-- Nome -->
+                        <div class="mt-4">
+                            <div class="inline-flex">
+        
+                                <x-label for="nome_arquivo" :value="__('Título do Documento')" /> <span class="ml-1 text-sm text-red-700">*</span>
+                            </div>
+        
+                            <x-input wire:model="nome_arquivo" id="nome_arquivo" class="block mt-1 w-full" type="text" name="nome"
+                                :value="old('nome_arquivo')" />
+                            @error('nome_arquivo') <span class="text-red-700">{{ $message }}</span> @enderror
+                        </div>
+                    </div>
+        
+                    <div class="col-span-6 sm:col-span-3">
+                        <!-- Arquivo -->
+                        <div class="mt-4">
+                            <div class="inline-flex">
+                                <x-label for="arquivo" :value="__('Arquivo')" /> <span
+                                    class=" ml-1 text-sm text-red-700">*</span>
+                            </div>
+        
+                            <x-input wire:model="arquivo" id="arquivo" class="input-file" type="file"
+                                name="arquivo" :value="old('arquivo')" />
+                            @error('arquivo') <span class="text-red-700">{{ $message }}</span>
+                            @enderror
+                        </div>
+                    </div>
+        
+                </div>
+            </div>
+            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6 ">
+                <a href="{{ url()->previous() }}" type="button"
+                    class="py-auto mr-4 inline-flex justify-center py-2 px-4 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Voltar
+                </a>
+                <button type="submit" wire:click="updateData"
+                    class="py-auto inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Salvar
+                </button>
+            </div>
+        </div>
+        
+
+        @endif
                 </div>
             </div>
         </div>
